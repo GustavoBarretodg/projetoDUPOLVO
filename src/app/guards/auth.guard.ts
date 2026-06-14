8f2c3cdf24026b80e43ad9cbd04578a5778b13d1
Gustavo Barreto <gmichell48@gmail.com>
2026-06-14 13:54:22 -0300

    Add AuthGuard to protect authenticated routes

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private storage: StorageService, private router: Router) {}
+
+  async canActivate(): Promise<boolean> {
+    const user = await this.storage.get('user');
+    if (user && user.id) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
